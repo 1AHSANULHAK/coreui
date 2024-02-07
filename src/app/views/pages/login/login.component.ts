df21a242c861e8e392538545971afb37c0c230d8
Ahsanulhak <96870004+1AHSANULHAK@users.noreply.github.com>
2024-02-07 22:39:03 +0530

    hjhjjhhjjh

--- src/app/views/pages/login/login.component.ts
+++ src/app/views/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,5 +9,10 @@ import { Component } from '@angular/core';
 export class LoginComponent {
 
   constructor() { }
+
+
+formss(form:ngForm){
+
+}
 
 }
